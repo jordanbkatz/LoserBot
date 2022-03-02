5fd6cf824cdd8030aa9bd75845d1af363c4d37d8
Jordan <jordanbkatz4@gmail.com>
2022-03-02 16:45:43 -0500

    second commit

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,17 @@
+"use strict";
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const dotenv = require('dotenv');
-const prefix: string = "$loser";
+const prefix = "$loser";
 dotenv.config();
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on("message", (message: any) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot)
+        return;
     const [cmd, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
     console.log(message);
 });
-
 client.login(process.env.TOKEN);
