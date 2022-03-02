3ccefcde5e75086a6fce900ce1ec320946610fb2
Jordan <jordanbkatz4@gmail.com>
2022-03-02 18:07:13 -0500

    started using .env instead of config.ts

--- dist/index.js
+++ dist/index.js
@@ -5,20 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = __importDefault(require("discord.js"));
 const client = new discord_js_1.default.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const config_1 = require("./config");
-try {
-    client.login(config_1.Config.token);
-}
-catch (e) {
-    console.log(`Login failed: ${e}`);
-}
+const prefix = "$loser";
+(require('dotenv')).config();
+client.login(process.env.TOKEN);
 client.on("ready", () => {
     var _a;
     console.log(`Logged in as ${(_a = client.user) === null || _a === void 0 ? void 0 : _a.tag}!`);
 });
 client.on("messageCreate", (message) => {
-    if (!message.content.startsWith(config_1.Config.prefix) || message.author.bot)
+    if (!message.content.startsWith(prefix) || message.author.bot)
         return;
-    const [cmd, ...args] = message.content.trim().substring(config_1.Config.prefix.length).split(/\s+/);
+    const [cmd, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
     message.reply("im a bot");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import Discord from 'discord.js';
 const client: Discord.Client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
-import { Config } from './config';
-try {
-    client.login(Config.token);
-}
-catch(e) {
-    console.log(`Login failed: ${e}`);
-}
+const prefix: string = "$loser";
+(require('dotenv')).config();
+client.login(process.env.TOKEN);
 client.on("ready", () => {
     console.log(`Logged in as ${client.user?.tag}!`);
 });
 client.on("messageCreate", (message: Discord.Message) => {
-    if (!message.content.startsWith(Config.prefix) || message.author.bot) return;
-    const [cmd, ...args] = message.content.trim().substring(Config.prefix.length).split(/\s+/);
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const [cmd, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
     message.reply("im a bot");
 });
